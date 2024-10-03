bc3b8251487bbeb1c3c483e195b403f750379dd9
Rik <rik.hoekstra@xs4all.nl>
2024-10-03 15:10:46 +0200

    new jobscripts

---
+++ lib/RikFlow/job_scripts/create_training_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J HF_sim
+#SBATCH -t 45:00
+#SBATCH -p gpu
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+
+module load 2023
+module load Julia/1.10.4-linux-x86_64
+
+julia --project experiment/HF_ref.jl
