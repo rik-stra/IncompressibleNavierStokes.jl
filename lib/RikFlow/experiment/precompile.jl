a8eaba4b82579da5b26f9d7cfd48a27bf4a82c27
Rik <rik.hoekstra@xs4all.nl>
2024-10-15 17:39:50 +0200

    full problem does not fit

--- lib/RikFlow/experiment/precompile.jl
+++ lib/RikFlow/experiment/precompile.jl
@@ -49,30 +49,3 @@ ispath(outdir) || mkpath(outdir)
 T = Float32
 ArrayType = CuArray
 #device = x -> adapt(CuArray, x)
-tburn = Float32(20)
-ustart = ArrayType.(load(outdir*"/u_start_spinnup_$(n_dns)_Re$(Re)_tsim$(tburn).jld2", "u_start"));
-
-# Parameters
-get_params(nlesscalar) = (;
-    D = 3,
-    Re,
-    lims = ( (T(0) , T(3)) , (T(0) , T(1)), (T(0),T(1)) ),
-    qois = [["Z",0,15],["E", 0, 15],["Z",16,31],["E", 16, 31]],
-    tsim,
-    Δt,
-    nles = map(n -> (3*n, n, n), nlesscalar), # LES resolutions
-    ndns = (n -> (3*n, n, n))(n_dns), # DNS resolution
-    filters = (FaceAverage(),),
-    ArrayType,
-    ustart,
-    bodyforce = (dim, x, y, z, t) -> (dim == 1) * 0.5 * sinpi(2*y)
-)
-
-params_train = (; get_params([n_les])..., savefreq = 1000);
-t3 = time()
-data_train = create_ref_data(; params_train...);
-t4 = time()
-println("HF simulation done. Time: $(t4-t3) s")
-# Save filtered DNS data
-#filename = "$outdir/data_train_dns$(n_dns)_les$(n_les)_Re$(Re)_tsim$(params_train.tsim).jld2"
-#jldsave(filename; data_train, params_train)
