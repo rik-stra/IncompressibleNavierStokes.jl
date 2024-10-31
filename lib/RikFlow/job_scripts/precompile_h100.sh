6e43d60037efb338e8d5daba85ebe340f730a2b0
Rik <rik.hoekstra@xs4all.nl>
2024-10-31 15:49:25 +0100

    precompilation for h100

--- lib/RikFlow/job_scripts/precompile_h100.sh
+++ lib/RikFlow/job_scripts/precompile_h100.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#SBATCH -J spinnup
+#SBATCH -J precompile
 #SBATCH -t 45:00
 #SBATCH --partition=gpu_h100
 #SBATCH --gpus=1
 
+mkdir -p /scratch-shared/$USER
+
 module load 2023
 module load Julia/1.10.4-linux-x86_64
+
 export JULIA_DEPOT_PATH=/scratch-shared/$USER/.julia_h100:
-julia --project exp_square_HIT/spinnup.jl
+
+julia --project -t auto -e 'using Pkg; Pkg.update()'
+julia --project exp_square_HIT/precompile.jl
