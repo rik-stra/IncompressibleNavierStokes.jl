b72d6a3ae5a3a5e9a6de1668eb8137a4d27303d5
Rik <rik.hoekstra@xs4all.nl>
2024-11-01 10:47:16 +0100

    ignore cache in module load

--- lib/RikFlow/job_scripts/precompile_h100.sh
+++ lib/RikFlow/job_scripts/precompile_h100.sh
@@ -7,7 +7,7 @@
 mkdir -p /scratch-shared/$USER
 
 module load 2023
-module load juliaup/1.14.5-GCCcore-12.3.0
+module --ignore_cache load juliaup/1.14.5-GCCcore-12.3.0
 
 export JULIA_DEPOT_PATH=/scratch-shared/$USER/.julia_h100:
 
