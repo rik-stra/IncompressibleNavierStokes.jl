f4f7659d7534b288b99eb7ec4b4b05c9d1190737
Rik <rik.hoekstra@xs4all.nl>
2024-11-01 11:18:48 +0100

    frist DNS setup

--- lib/RikFlow/job_scripts/spinnup.sh
+++ lib/RikFlow/job_scripts/spinnup.sh
@@ -4,7 +4,5 @@
 #SBATCH --partition=gpu_h100
 #SBATCH --gpus=1
 
-module load 2023
-module load Julia/1.10.4-linux-x86_64
 export JULIA_DEPOT_PATH=/scratch-shared/$USER/.julia_h100:
 julia --project exp_square_HIT/spinnup.jl
