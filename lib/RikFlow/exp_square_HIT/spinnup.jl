042051f310b08ad3626add941589d10dcd493b6e
Rik <rik.hoekstra@xs4all.nl>
2024-11-06 13:53:08 +0100

    spinnup with freeze

--- lib/RikFlow/exp_square_HIT/spinnup.jl
+++ lib/RikFlow/exp_square_HIT/spinnup.jl
@@ -34,8 +34,8 @@ println("Modules loaded. Time: $(t1-t0) s")
 n_dns = Int(512)
 n_les = Int(64)
 Re = Float32(2_000)
-tburn = Float32(2)
-#Δt = Float32(0.00025)
+tburn = Float32(4)
+Δt = Float32(0.00025)
 
 #n_dns = Int(128)
 #n_les = Int(64)
@@ -46,13 +46,13 @@ tburn = Float32(2)
 T_L = 0.01  # correlation time of the forcing
 e_star = 0.1 # energy injection rate
 k_f = sqrt(2) # forcing wavenumber
-freeze = 1 # number of time steps to freeze the forcing
+freeze = 10 # number of time steps to freeze the forcing
 
 outdir = @__DIR__() *"/output"
 ispath(outdir) || mkpath(outdir)
 
 seeds = (;
-    dns = 123, # DNS initial condition
+    ou_spin = 123, # DNS initial condition
     ou = 333, # OU process
     to = 234, # TO method online sampling
 )
@@ -70,7 +70,7 @@ get_params(nlesscalar) = (;
     tburn,
     ndns = (n -> (n, n, n))(n_dns), # DNS resolution
     ArrayType,
-    ou_bodyforce = (;T_L, e_star, k_f, freeze, rng_seed = seeds.ou ),
+    ou_bodyforce = (;T_L, e_star, k_f, freeze, rng_seed = seeds.ou_spin ),
 )
 
 params_train = (; get_params([n_les])...);
@@ -81,9 +81,9 @@ u_start = Array.(u_start);
 t4 = time()
 println("HF simulation done. Time: $(t4-t3) s")
 # Save filtered DNS data
-filename = "$outdir/u_start_spinnup_$(n_dns)_Re$(Re)_tsim$(params_train.tburn).jld2"
+filename = "$outdir/u_start_spinnup_$(n_dns)_Re$(Re)_freeze_$(freeze)_tsim$(params_train.tburn).jld2"
 jldsave(filename; u_start)
 
 # Plot
-save(outdir*"/ehist2_$(n_dns)_Re$(Re)_tsim$(params_train.tburn).png",ehist)
+save(outdir*"/ehist2_$(n_dns)_Re$(Re)_freeze_$(freeze)_tsim$(params_train.tburn).png",ehist)
 #save(outdir*"/espec_$(n_dns)_Re$(Re)_tsim$(params_train.tburn).png",espec)
