3cde845641bc8b69e510d0fa4557c6736148e00b
Rik <rik.hoekstra@xs4all.nl>
2024-12-11 10:28:01 +0100

    setup linreg search

--- lib/RikFlow/exp_square_HIT/LinReg_search/setup_search.jl
+++ lib/RikFlow/exp_square_HIT/LinReg_search/setup_search.jl
@@ -1,21 +1,24 @@
 using JLD2
 using DataFrames
 
-fixed_parameters = (track_file = "/../ANN_search/data_track.jld2",
+fixed_parameters = (track_file = "/data_track.jld2",
                     hist_len = 5,
                     hist_var = :q_star,
                     n_replicas = 10,
                     normalization = :standardise)
 
-varying_parameters = (hist_len = [0, 1, 3, 10, 20, 50],)
+varying_parameters = (hist_len = [0, 1, 3, 5, 10, 20, 50], hist_var = [:q, :q_star])
 
 
-i = 1
-inputs = [(name = "LinReg$i", fixed_parameters...)]
-for h in varying_parameters.hist_len
-    i += 1
-    push!(inputs, (name = "LinReg$i", fixed_parameters..., hist_len = h))
+i = 0
+inputs = []
+for hv in varying_parameters.hist_var
+    for h in varying_parameters.hist_len
+        i += 1
+        push!(inputs, (name = "LinReg$i", fixed_parameters..., hist_len = h, hist_var = hv))
+    end
 end
+
 
 save(@__DIR__()*"/inputs.jld2", "inputs", inputs)
 inputs_df = DataFrame(inputs)
