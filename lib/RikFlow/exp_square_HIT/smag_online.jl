875aef1623124bfe52904b4265e92ce4a79b0e24
Rik <rik.hoekstra@xs4all.nl>
2025-01-09 16:48:34 +0100

    perform long smag sims

--- lib/RikFlow/exp_square_HIT/smag_online.jl
+++ lib/RikFlow/exp_square_HIT/smag_online.jl
@@ -14,15 +14,16 @@ using RikFlow
 using IncompressibleNavierStokes
 using CUDA
 
+smag_vals = collect(0.05:0.005:0.1)
 n_dns = Int(512)
 n_les = Int(64)
 Re = Float32(2_000)
 ############################
-Δt = Float32(2.5e-3)
-tsim = Float32(10)
+Δt = Float32(2.5f-3)
+tsim = Float32(100)
 # forcing
-T_L = 0.01  # correlation time of the forcing
-e_star = 0.1 # energy injection rate
+T_L = 0.01f0  # correlation time of the forcing
+e_star = 0.1f0 # energy injection rate
 k_f = sqrt(2) # forcing wavenumber  
 freeze = 1 # number of time steps to freeze the forcing
 
@@ -34,18 +35,25 @@ seeds = (;
 
 outdir = @__DIR__() *"/output/new"
 ispath(outdir) || mkpath(outdir)
+ispath(outdir*"/smag") || mkpath(outdir*"/smag")
+
 
 # For running on a CUDA compatible GPU
 
 T = Float32
 ArrayType = CuArray
+backend = CUDABackend()
 
 # load reference data
 track_file = outdir*"/data_track_dns$(n_dns)_les$(n_les)_Re$(Re)_tsim10.0.jld2"
 data_track = load(track_file, "data_track");
 params_track = load(track_file, "params_track");
 # get initial condition
-ustart = ArrayType.(data_track.fields[1].u);
+if data_track.fields[1].u isa Tuple
+    ustart = stack(ArrayType.(data_track.fields[1].u));
+elseif data_track.fields[1].u isa Array{<:Number,4}
+    ustart = ArrayType(data_track.fields[1].u);
+end
 
 
 params = (;
@@ -55,45 +63,51 @@ params = (;
     ArrayType,
     ustart, 
     #ou_bodyforce = (;T_L, e_star, k_f, freeze, rng_seed = seeds.ou),
-    savefreq = 100);
+    savefreq = 1000);
 
-# Build setup and assemble operators
-setup = 
-Setup(;
-    x = ntuple(α -> LinRange(params.lims[α]..., params.nles[1][α] + 1), params.D),
-    Re=params.Re,
-    ArrayType,
-    params.ou_bodyforce,
-    closure_model = smagorinsky_closure_natural(setup)
-);
 
-# Number of time steps to save
-nt = round(Int, params.tsim / params.Δt)
+for c_s in smag_vals
+    # Build setup and assemble operators
+    setup = Setup(;
+        x = ntuple(α -> LinRange(params.lims[α]..., params.nles[1][α] + 1), params.D),
+        Re=params.Re,
+        ArrayType,
+        backend,
+        params.ou_bodyforce,
+    );
+    closure_model = IncompressibleNavierStokes.smagorinsky_closure_natural(setup)
+    setup = (; setup..., closure_model)
 
-to_setup_les = RikFlow.TO_Setup(; 
-         params.qois, 
-         to_mode = :CREATE_REF,
-         params.ArrayType, 
-         setup,
-         nstep=nt);
+    # Number of time steps to save
+    nt = round(Int, params.tsim / params.Δt)
 
-psolver = psolver_spectral(setup);
+    to_setup_les = RikFlow.TO_Setup(; 
+            params.qois, 
+            to_mode = :CREATE_REF,
+            params.ArrayType, 
+            setup,
+            nstep=nt);
 
-# Solve
-@info "Solving LF sim (no SGS)"
-(; u, t), outputs =
-        solve_unsteady(; setup,
-        θ = 0.17, 
-        params.ustart, 
-        tlims = (T(0), params.tsim),
-        params.Δt,
-        processors = (;
-            log = timelogger(; nupdate = 100),
-            fields = fieldsaver(; setup, nupdate = params.savefreq),  # by calling this BEFORE qoisaver, we also save the field at t=0!
-            qoihist = RikFlow.qoisaver(; setup, to_setup=to_setup_les, nupdate = 1),
-        ),
-        psolver);
-q = stack(outputs.qoihist);
-data_online = (;q, fields = outputs.fields);
-# Save tracking data
-jldsave("$outdir/data_smag_dns$(n_dns)_les$(n_les)_Re$(Re)_tsim$(tsim).jld2"; data_online, params);
+    psolver = psolver_spectral(setup);
+
+    # Solve
+    @info "Solving LF sim (SMAG)"
+    (; u, t), outputs = solve_unsteady(; 
+            setup,
+            θ = T(c_s), 
+            ustart,
+            tlims = (T(0), params.tsim),
+            params.Δt,
+            processors = (;
+                log = timelogger(; nupdate = 100),
+                fields = fieldsaver(; setup, nupdate = params.savefreq),  # by calling this BEFORE qoisaver, we also save the field at t=0!
+                qoihist = RikFlow.qoisaver(; setup, to_setup=to_setup_les, nupdate = 1),
+            ),
+            psolver,
+    );
+
+    q = stack(outputs.qoihist);
+    data_online = (;q, fields = outputs.fields);
+    # Save tracking data
+    jldsave("$outdir/smag/data_smag_$(c_s)_dns$(n_dns)_les$(n_les)_Re$(Re)_tsim$(tsim).jld2"; data_online, params);
+end
