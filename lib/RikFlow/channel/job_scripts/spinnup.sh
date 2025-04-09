d9516f8be397800e256eb0ee4b5ced96f9a18b5c
Rik <rik.hoekstra@xs4all.nl>
2025-04-09 15:54:55 +0200

    add jobfile

---
+++ lib/RikFlow/channel/job_scripts/spinnup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J spinnup
+#SBATCH -t 2:00:00
+#SBATCH --partition=gpu_h100
+#SBATCH --gpus=1
+
+export JULIA_DEPOT_PATH=/scratch-shared/$USER/.julia_h100:
+julia --project -t auto -e 'using Pkg; Pkg.update()'
+julia --project channel_spinnup.jl
