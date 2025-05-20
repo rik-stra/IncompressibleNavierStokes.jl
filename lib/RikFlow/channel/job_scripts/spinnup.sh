d2696c84b4e253f4cdf42d26218e2ced30c443a6
Rik <rik.hoekstra@xs4all.nl>
2025-05-20 10:19:18 +0200

    correct julia path

--- lib/RikFlow/channel/job_scripts/spinnup.sh
+++ lib/RikFlow/channel/job_scripts/spinnup.sh
@@ -4,6 +4,6 @@
 #SBATCH --partition=gpu_h100
 #SBATCH --gpus=1
 
-export JULIA_DEPOT_PATH=/scratch-shared/$USER/.julia_h100:
+export JULIA_DEPOT_PATH=$HOME/julia/julia_h100:
 # julia --project -t auto -e 'using Pkg; Pkg.update()'
 julia --project channel_spinnup.jl
