b58f282aff77bbdad02650ce755b4f133b3389d2
Rik <rik.hoekstra@xs4all.nl>
2025-06-03 10:29:54 +0200

    get restart field

--- lib/RikFlow/channel/channel_HF.jl
+++ lib/RikFlow/channel/channel_HF.jl
@@ -30,7 +30,7 @@ xlims = 0f, 4f * pi
 ylims = 0f, 2f
 zlims = 0f, 4f / 3f * pi
 
-tsim =0.02f
+tsim = 5f
 # Grid
 nx = 512      #-> highest wave number 128/4pi = 10.2
 ny = 512      #-> highest wave number 128/2 = 64
