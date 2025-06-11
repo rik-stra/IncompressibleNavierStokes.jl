a8818e3876b19e2a5a0406581b520c77954f13c0
Rik <rik.hoekstra@xs4all.nl>
2025-06-11 16:14:18 +0200

    compks jobfile

---
+++ lib/RikFlow/channel/job_scripts/comp_ks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J compks
+#SBATCH -t 20:00
+#SBATCH --partition=gpu_h100
+#SBATCH --gpus=1
+
+export JULIA_DEPOT_PATH=$HOME/julia/julia_h100:
+# julia --project -t auto -e 'using Pkg; Pkg.update()'
+julia --project compute_ks.jl
