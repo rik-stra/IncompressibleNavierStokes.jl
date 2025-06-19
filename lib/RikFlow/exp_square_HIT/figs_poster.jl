b5ce54275333989c1eb61214e59713347368e63b
Rik <rik.hoekstra@xs4all.nl>
2025-06-19 18:04:46 +0200

    update poster figs

--- lib/RikFlow/exp_square_HIT/figs_poster.jl
+++ lib/RikFlow/exp_square_HIT/figs_poster.jl
@@ -75,33 +75,37 @@ save(fig_folder*"/energy_spectrum_afterspinup.pdf", fig)
 smag_data = load(
             @__DIR__()*"/output/new/smag/data_smag_0.071_dns512_les64_Re2000.0_tsim100.0.jld2",
             "data_online").q;
+fname = @__DIR__()*"/output/new/data_train_dns512_les64_Re2000.0_freeze_10_tsim100.0.jld2"
 q_ref = stack(load(fname, "data_train").data[1].qoi_hist);
 linreg_data = [load(
         @__DIR__()*"/paper_runs/output/online/LinReg1/data_online_dns512_les64_Re2000.0_tsim100.0_replica$(i)_rand_initial_dQ.jld2",
         "data_online").q for i in 1:5]
 qs = cat(linreg_data..., dims = 2)
+qois = [["Z",0,6],["E", 0, 6],["Z",7,15],["E", 7, 15],["Z",16,32],["E", 16, 32]]
 let 
-        g = Figure()
-        axs = [Axis(g[i ÷ 2, i%2], 
+        g = Figure(size = (800, 300))
+        axs = [Axis(g[i % 2, i÷2], 
         title = L"%$(qois[i+1][1])_{[%$(qois[i+1][2]), %$(qois[i+1][3])]}")
             for i in 0:size(smag_data, 1)-1]
+        ref, to, smag = nothing, nothing, nothing
         for i in 1:size(smag_data, 1)
-            density!(axs[i], q_ref[i, :], label = "ref", color = (:black, 0.3),
+            ref = density!(axs[i], q_ref[i, :], label = "ref", color = (:black, 0.3),
             strokecolor = :black, strokewidth = 3, strokearound = false)
             
-            density!(axs[i], qs[i, :], label = "TO", color = (:teal, 0.3),
+            to = density!(axs[i], qs[i, :], label = "TO", color = (:teal, 0.3),
                 strokecolor = :teal, strokewidth = 3, linestyle=:dot, strokearound = false)
 
-            density!(axs[i], smag_data[i, :], label = "Smag", color = (:orange, 0.3),
+            smag = density!(axs[i], smag_data[i, :], label = "Smag", color = (:orange, 0.3),
             strokecolor = :orange, strokewidth = 3, linestyle = :dot, strokearound = false)
 
             hideydecorations!(axs[i], label = false, ticks = false, grid = false)
-            if i == size(smag_data, 1) axislegend(axs[i], position = :rt) end
-            if i in [1,3,5]
+            #if i == size(smag_data, 1) axislegend(axs[i], position = :rt) end
+            #Legend(g[:,3], [ref, to, smag], ["Reference", "TO", "Smagorinsky"])
+            if i in [1,2]
                 axs[i].ylabel="Density"
             end
         end
 
         display(g)
-        #save(fig_folder*"/lt_distr_q_smag_dns512_les64_Re2000.0_tsim100.png", g)
+        save(fig_folder*"/lt_distr_q_poster.pdf", g)
 end
